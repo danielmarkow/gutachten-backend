6ed68d9efee47d34299cfa4c89aa712fad52c34d
Daniel Markow <d_markow@icloud.com>
2023-05-19 08:14:01 +0200

    added description field

--- schemas.py
+++ schemas.py
@@ -1,23 +1,17 @@
-# {
-#   "root":
-#     {
-#       "children": [
-#         {
-#           "children":[
-#             {"detail":0,"format":0,"mode":"normal","style":"","text":"hello world","type":"text","version":1}
-#           ],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},
-# {"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"blub","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}
 import uuid as uuid_pkg
-# from pydantic import BaseModel
+from typing import Optional
 from sqlmodel import SQLModel, Field, JSON, Column
 
 class GutachtenInput(SQLModel):
     ga: dict
+    description: Optional[str]
 
 class GutachenOutput(GutachtenInput):
     id: uuid_pkg.UUID
     ga: dict
+    description: Optional[str]
 
 class Gutachten(GutachtenInput, table=True):
     id: uuid_pkg.UUID | None = Field(default_factory=uuid_pkg.uuid4, primary_key=True, index=True, nullable=False)
     ga: dict = Field(sa_column=Column(JSON), default={})
+    description: Optional[str] = Field(default=None)
